3eb8b7079d037d87b43ead9d93890e835c05855d
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-05-06 22:37:43 +0300

    added the header component with some styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { ErrorComponent } from './components/error component/error/error.compone
 import { HomeComponent } from './components/home component/home/home.component';
 import { MonsterComponent } from './components/monster component/monster/monster.component';
 import { MonstersComponent } from './components/monsters component/monsters/monsters.component';
+import { HeaderComponent } from './components/header component/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, MonsterDetailsComponent, ErrorComponent, HomeComponent, MonsterComponent, MonstersComponent],
+  declarations: [AppComponent, MonsterDetailsComponent, ErrorComponent, HomeComponent, MonsterComponent, MonstersComponent, HeaderComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
