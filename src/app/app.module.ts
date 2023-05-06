ac130e6b404ed375866f7672a5351bb670efb55c
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-05-06 22:23:27 +0300

    added the starter files and generated the components starters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app component/app.component';
+import { MonsterDetailsComponent } from './components/details component/monster-details/monster-details.component';
+import { ErrorComponent } from './components/error component/error/error.component';
+import { HomeComponent } from './components/home component/home/home.component';
+import { MonsterComponent } from './components/monster component/monster/monster.component';
+import { MonstersComponent } from './components/monsters component/monsters/monsters.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, MonsterDetailsComponent, ErrorComponent, HomeComponent, MonsterComponent, MonstersComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
