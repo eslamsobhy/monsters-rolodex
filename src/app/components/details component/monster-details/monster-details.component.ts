e8a290901c8dadca46c61e3a2effcbe1eb3636a3
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-05-07 15:05:38 +0300

    fetched the monsters details for the clicked monster and displayed it

--- src/app/components/details component/monster-details/monster-details.component.ts
+++ src/app/components/details component/monster-details/monster-details.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MonstersServiceService } from 'src/app/services/monsters-service.service';
 
 @Component({
   selector: 'app-monster-details',
   templateUrl: './monster-details.component.html',
-  styleUrls: ['./monster-details.component.css']
+  styleUrls: ['./monster-details.component.css'],
 })
-export class MonsterDetailsComponent {
+export class MonsterDetailsComponent implements OnInit {
+  id: any;
+  monster: any;
 
+  constructor(
+    myRoute: ActivatedRoute,
+    private monstersService: MonstersServiceService
+  ) {
+    this.id = myRoute.snapshot.params['id'];
+  }
+
+  ngOnInit(): void {
+    this.monstersService.getMonsterById(this.id).subscribe(
+      (data) => {
+        this.monster = data;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
 }
