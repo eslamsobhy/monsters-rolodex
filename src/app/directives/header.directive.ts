d8b2c9be88f67aa80b582668a5851e0753dba989
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-05-30 18:17:06 +0300

    handled the hover-event in the custom attribute-directive

--- src/app/directives/header.directive.ts
+++ src/app/directives/header.directive.ts
@@ -1,11 +1,21 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHeader]',
 })
 export class HeaderDirective {
-  constructor(eleRef: ElementRef) {
+  constructor(private eleRef: ElementRef) {
     // console.log(eleRef.nativeElement);
     eleRef.nativeElement.style.color = '#0ccac4';
+  }
+
+  @HostListener('mouseenter')
+  onMouseEnter() {
+    this.eleRef.nativeElement.style.color = '#68e7e3';
+  }
+
+  @HostListener('mouseleave')
+  onMouseLeave() {
+    this.eleRef.nativeElement.style.color = '#0ccac4';
   }
 }
